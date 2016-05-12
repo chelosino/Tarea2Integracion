8632d74ad52664c09ef0673a7e7c46463836c180
chelosino <marcelbehar@MBP-de-Marcel.iptv.microsoft.com>
2016-05-12 17:04:42 -0300

    Update test.rb

--- test.rb
+++ test.rb
@@ -9,29 +9,35 @@ def app
   Sinatra::Application
 end
 
-describe "Tarea 2" do
+describe "Tarea2" do
 
-  it "should return the factors of 6" do
-    6.factors.must_equal [1,2,3,6]
-  end
+  #it "should return the factors of 6" do
+  #  6.factors.must_equal [1,2,3,6]
+  #end
+#
+  #it "should say that 2 is prime" do
+  #  assert 2.prime?
+  #end
+#
+  #it "should say that 10 is not prime" do
+  #  refute 10.prime?
+  #end
+#
+  #it "should return json" do
+  #  get '/6'
+  #  last_response.headers['Content-Type'].must_equal 'application/json;charset=utf-8'
+  #end 
+#
+  #it "should return the correct info about 6 as json" do
+  #  get '/6'
+  #  six_info = { number: 6, factors: 6.factors, odd: 6.odd?, even: 6.even?, prime: 6.prime? }
+  #  six_info.to_json.must_equal last_response.body
+  #end
 
-  it "should say that 2 is prime" do
-    assert 2.prime?
-  end
-
-  it "should say that 10 is not prime" do
-    refute 10.prime?
-  end
-
-  it "should return json" do
-    get '/6'
-    last_response.headers['Content-Type'].must_equal 'application/json;charset=utf-8'
-  end 
-
-  it "should return the correct info about 6 as json" do
-    get '/6'
-    six_info = { number: 6, factors: 6.factors, odd: 6.odd?, even: 6.even?, prime: 6.prime? }
-    six_info.to_json.must_equal last_response.body
+  it "should retun chao" do
+    get '/hola'
+    info = 'chao'
+    info.must_equal last_response.body
   end
 
 end
