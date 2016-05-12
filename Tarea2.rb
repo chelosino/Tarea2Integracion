af4ede5abde833dfff77a2b242e21c047a674171
chelosino <marcelbehar@MBP-de-Marcel.iptv.microsoft.com>
2016-05-12 18:21:04 -0300

    Método conecta con Instagram

--- Tarea2.rb
+++ Tarea2.rb
@@ -1,6 +1,22 @@
 require 'sinatra'
 require 'json'
+require 'open-uri'
 
-get '/hola' do
-	return 'chao'
+get '/' do
+	return 'Hello World!'
+end
+
+post '/instagram/tag/buscar' do
+	tag = params['tag']
+	access_token = params['access_token']
+
+	if not params['tag'] or not params['access_token']
+		return status 400
+	end
+
+	begin
+		status = 200
+		datos = open('https://api.instagram.com/v1/tags/' + tag + '/media/recent?access_token=' + access_token + '')
+		return datos
+	end
 end
