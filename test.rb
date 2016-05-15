40ef36d504657c5d24b350159bed27248c662439
chelosino <marcelbehar@MacBook-Pro-de-Marcel.local>
2016-05-15 12:06:43 -0300

    Add cover

--- test.rb
+++ test.rb
@@ -41,7 +41,7 @@ describe "Tarea2" do
   #end
 
   it "should retun 200" do
-    get "/"
+    post '/instagram/tag/buscar?tag=santiago&access_token=2019746130.59a3f2b.86a0135240404ed5b908a14c0a2d9402'
     status = 200
     status.must_equal last_response.status
   end
